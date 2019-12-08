a85d8596fdb556e64d8ec2605011c84b02934764
4masaka <in9lude@gmail.com>
2019-12-09 08:42:47 +0900

    deep copy

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,13 +1,6 @@
 fn main() {
-    for i in 1..101 {
-        if i % 15 == 0 {
-            println!("FizzBuzz");
-        } else if i % 5 == 0 {
-            println!("Buzz");
-        } else if i % 3 == 0 {
-            println!("Fizz");
-        } else {
-            println!("{}", i);
-        }
-    }
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
